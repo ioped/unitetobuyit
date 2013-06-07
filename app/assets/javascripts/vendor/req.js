3203dac04d03b668e23f949b013ef4090aa8f426
Pedro Maltez <github@ped.io>
2013-06-07 12:59:03 +0100

    Generate Rails application.
    Modify
      existing files to fit rails structure.
    
    Delete
      some unnecessary Rails files and gems.
      public/index.html
      jquery-rails gem
    
    Create
      Site controller and index action.
      site#index route.
      JavaScript requirement files.

---
+++ app/assets/javascripts/vendor/req.js
@@ -0,0 +1,4 @@
+//= require ./jquery-2.0.2.js
+//= require ./handlebars-1.0.0-rc.4.js
+//= require ./ember-1.0.0-rc.5.js
+//= require ./ember-data-0.13.js
---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,7 @@
+class SiteController < ApplicationController
+  protect_from_forgery
+
+  def index
+    
+  end
+end
