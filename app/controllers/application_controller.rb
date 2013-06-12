2eb669cae6e83239ea42674c3cc92ec4f1133eb0
Pedro Maltez <github@ped.io>
2013-06-13 00:18:07 +0100

    Modify ember.js router, routes, and store.
    Convert to from fixtures to RESTAdapter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  respond_to :json
 
-  def index
-  end
+  helper_method :current_user
+  helper_method :authorize
+
+  private
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authorize
+      unless current_user
+        redirect_to login_url, alert: "Please log in."
+      end
+    end
 
 end
