9507d153ed8e8a77e96f4176b8f2a3a310f36a4c
Pedro Maltez <github@ped.io>
2013-06-13 00:15:51 +0100

    Modify configuration files.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ end
 
 module UniteToBuyIt
   class Application < Rails::Application
+    Mongoid.logger.level = Logger::DEBUG
+    Moped.logger.level = Logger::DEBUG
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,19 @@
 UniteToBuyIt::Application.routes.draw do
 
-  root :to => 'application#index'
+  root :to => 'site#index'
 
-  # scope '/api' do
-  # end
+  get '/signup' => 'users#new', as: :signup
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  get '/login' => 'sessions#new', as: :login
+  get '/logout' => 'sessions#destroy', as: :logout
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  resources :sessions
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  scope '/api/' do
+    resources :users, except: [ :new, :edit ], defaults: { format: :json }
+    resources :addresses, except: [:new, :edit ], defaults: { format: :json }
+    resources :group_buys, except: [ :new, :edit ], defaults: { format: :json }
+    resources :orderable_sets, except: [ :new, :edit ], defaults: { format: :json }
+    resources :orders, except: [ :new, :edit ], defaults: { format: :json }
+  end
 end
