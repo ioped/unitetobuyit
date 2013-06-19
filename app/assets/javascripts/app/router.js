4603797d753b346f0a192100486e36f4ac6b0dac
Pedro Maltez <github@ped.io>
2013-06-19 16:40:44 +0100

    Minor modifications to style.

--- app/assets/javascripts/app/router.js
+++ app/assets/javascripts/app/router.js
@@ -4,21 +4,25 @@ UniteToBuyIt.Router.map(function() {
     this.route('show', { path: ':user_id' });
     this.route('edit', { path: ':user_id/edit' });
   });
+
   this.resource('group_buys', function() {
     this.route('new');
     this.route('show', { path: ':group_buy_id' });
     this.route('edit', { path: ':group_buy_id/edit' });
   });
+
   this.resource('orders', function() {
     this.route('new');
     this.route('show', { path: ':orders_id' });
     this.route('edit', { path: ':orders_id/edit' });
   });
+
   this.resource('orderable_items', function() {
     this.route('new');
     this.route('show', { path: ':orderable_items_id' });
     this.route('edit', { path: ':orderable_items_id/edit' });
   });
+
   this.resource('orderable_sets', function() {
     this.route('new');
     this.route('show', { path: ':orderable_sets_id' });
@@ -28,6 +32,4 @@ UniteToBuyIt.Router.map(function() {
   this.route('about');
   this.route('faq');
   this.route('contact_us');
-
-
 });
